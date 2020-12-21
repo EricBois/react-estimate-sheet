b1c8de71044b513c8919a55263383b00a3367403
Eric Boisvenu <burn4live@gmail.com>
2020-12-21 09:22:05 -0700

    added router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Estimator from './Estimator';
+import CreateEstimateForm from './CreateEstimateForm';
+import { EstimatesProvider } from './context/estimate.context';
+import NavBar from './NavBar';
 
 function App() {
   return (
-    <Estimator />
+    <EstimatesProvider>
+      <Route path="/">
+        <NavBar />
+      <Switch>
+        <Route path="/create">
+          <CreateEstimateForm />
+        </Route>
+        <Route path="/">
+          <Estimator />
+        </Route>
+      </Switch>
+      </Route>
+    </EstimatesProvider>
   );
 }
 
