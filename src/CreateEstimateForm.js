93c0d109c83c8e6b13dd954f070ad9ed83ebbe33
Eric Boisvenu <burn4live@gmail.com>
2020-12-23 15:56:46 -0700

    updated styles and added uuid

--- src/CreateEstimateForm.js
+++ src/CreateEstimateForm.js
@@ -1,45 +1,86 @@
-import React, { useContext, Fragment } from 'react';
+import React, { useContext } from 'react';
+import { v4 as uuid } from 'uuid';
 import useInputState from './hooks/useInputState';
 import { DispatchContext } from './context/estimate.context';
 
+import { withStyles } from '@material-ui/styles';
+import styles from './styles/createEstimateStyles';
+
 import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
+import TextareaAutosize from '@material-ui/core/TextareaAutosize';
 import Button from '@material-ui/core/Button';
 
-function CreateEstimateForm() {
+function CreateEstimateForm(props) {
   const [name, handleChangeName, resetName] = useInputState('');
   const [address, handleChangeAddress, resetAddress] = useInputState('');
 
   const dispatch = useContext(DispatchContext);
+  const { classes } = props;
+
+  const handleSubmit = async () => {
+    const id = uuid()
+    await dispatch({ type: 'ADD', name, address, id });
+    resetName();
+    resetAddress();
+    console.log(id)
+  };
 
   return (
-    <Paper style={{ margin: '1rem 0', padding: '0 1rem' }}>
+    <Paper
+      elevation={3}
+      style={{
+        padding: '0px 15px',
+        margin: 'auto',
+        maxWidth: '850px',
+        height: '80vh',
+        backgroundColor: '#fafafa',
+        textAlign: 'center',
+      }}
+    >
+      <Typography className={classes.title} variant="h5">
+        Client Info
+      </Typography>
       <form
         onSubmit={(e) => {
           e.preventDefault();
-          dispatch({type: "ADD", name: name, address: address});
-          resetName();
-          resetAddress();
+          handleSubmit();
         }}
       >
-        <TextField
-          value={name}
-          onChange={handleChangeName}
-          margin="normal"
-          label="Name"
-          fullWidth
-        />
-        <TextField
-          value={address}
-          onChange={handleChangeAddress}
-          margin="normal"
-          label="Address"
-          fullWidth
-        />
-        <Button type="submit">Submit</Button>
+        <Grid container justify="center" spacing={2}>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              value={name}
+              onChange={handleChangeName}
+              margin="normal"
+              label="Name"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              value={address}
+              onChange={handleChangeAddress}
+              margin="normal"
+              label="Address"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextareaAutosize
+              className={classes.textArea}
+              aria-label="minimum height"
+              rowsMin={5}
+              placeholder="*OPTIONAL, Describe work / project"
+            />
+          </Grid>
+          <Button type="submit">Create</Button>
+        </Grid>
       </form>
     </Paper>
   );
 }
 
-export default CreateEstimateForm;
+export default withStyles(styles)(CreateEstimateForm);
