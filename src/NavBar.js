b7679ea78ff771615beff08901d1cf922d02f544
Eric Boisvenu <burn4live@gmail.com>
2020-12-23 15:57:06 -0700

    updated style

--- src/NavBar.js
+++ src/NavBar.js
@@ -22,7 +22,7 @@ const drawerWidth = 240;
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
-    marginBottom: '4rem'
+    marginBottom: '5rem'
   },
   appBar: {
     transition: theme.transitions.create(['margin', 'width'], {
