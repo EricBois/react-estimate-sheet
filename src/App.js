afc3d251c71e00be9966e822c523f2b6ca094786
Eric Boisvenu <burn4live@gmail.com>
2020-12-25 15:55:40 -0700

    refactored and added estimate

--- src/App.js
+++ src/App.js
@@ -3,20 +3,43 @@ import { Route, Switch } from 'react-router-dom';
 import Estimator from './Estimator';
 import CreateEstimateForm from './CreateEstimateForm';
 import NavBar from './NavBar';
+import Estimate from './Estimate';
+import estimateReducer from './reducers/estimate.reducer';
+import useLocalStorageReducer from './hooks/useLocalStorageReducer';
 
 function App() {
+  const [estimates, dispatch] = useLocalStorageReducer(
+    'estimates',
+    [],
+    estimateReducer
+  );
+
+
+  function findEstimate(id) {
+    return estimates.find(function (estimate) {
+      return estimate.id === id;
+    });
+  }
+  
   return (
-      <Route path="/">
-        <NavBar />
+    <Route path="/">
+      <NavBar />
       <Switch>
-        <Route path="/create">
-          <CreateEstimateForm />
+        <Route exact path="/create">
+          <CreateEstimateForm
+            estimates={estimates}
+            dispatch={(props) => dispatch(props)}
+          />
+          ;
         </Route>
-        <Route path="/">
-          <Estimator />
+
+        <Route exact path="/">
+          <Estimator estimates={estimates} />
         </Route>
+
+        <Route exact path="/estimate/:id" render={(routeProps) => <Estimate estimate={findEstimate(routeProps.match.params.id)} />}/>
       </Switch>
-      </Route>
+    </Route>
   );
 }
 
