8c713078cd5630cc645e2ff3cf0645d5d2abfa55
Eric Boisvenu <burn4live@gmail.com>
2020-12-27 15:49:13 -0700

    added a few reducers

--- src/reducers/estimate.reducer.js
+++ src/reducers/estimate.reducer.js
@@ -1,14 +1,32 @@
-
 const reducer = (state, action) => {
-  switch(action.type){
-    case "ADD":
-      return [...state, { id: action.id, name: action.name, address: action.address }]
-    // case "REMOVE":
-    //   return state.filter((todo) => todo.id !== action.id)
-    // case "TOGGLE":
-    //   return state.map((todo) => todo.id === action.id ? { ...todo, completed: !todo.completed } : todo )
-    // case "EDIT":
-    //   return state.map((todo) => todo.id === action.id ? { ...todo, task: action.newTask } : todo )
+  switch (action.type) {
+    case 'ADD':
+      return [
+        ...state,
+        {
+          id: action.id,
+          name: action.name,
+          address: action.address,
+          note: action.note,
+          measures: [],
+        },
+      ];
+    case 'REMOVE':
+      return state.filter((estimate) => estimate.id !== action.id);
+    case 'DELMEASURE':
+      return state.filter((estimate) => estimate.id === action.id ? estimate.measures.splice(action.index, 1) : estimate);
+    case 'EDIT':
+      return state.map((estimate) =>
+        estimate.id === action.id
+          ? {
+              ...estimate,
+              name: action.name,
+              address: action.address,
+              note: action.note,
+              measures: [...estimate.measures, action.measures],
+            }
+          : estimate
+      );
     default:
       return state;
   }
