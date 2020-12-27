874bd24df65411f9bbf321d4b854073a6df14f77
Eric Boisvenu <burn4live@gmail.com>
2020-12-27 15:49:47 -0700

    styled components

---
+++ src/styles/estimateStyles.js
@@ -0,0 +1,9 @@
+const styles = {
+  root: {
+    flexGrow: 1,
+    maxWidth: 650,
+    margin: 'auto'
+  },
+}
+
+export default styles;
---
+++ src/styles/measurementsStyles.js
@@ -0,0 +1,18 @@
+const styles = {
+  textfield: {
+    marginLeft: '1px'
+  },
+  button: {
+    position: 'relative',
+    top: '1.3em',
+    height: '68%',
+    width: '80%'
+  },
+  title: {
+    fontWeight: 'bold',
+    marginBottom: '-1em'
+    
+  }
+}
+
+export default styles;
