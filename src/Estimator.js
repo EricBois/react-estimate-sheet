b262e71bf0e7f584c0494d44f165794627a946aa
Eric Boisvenu <burn4live@gmail.com>
2020-12-28 14:22:35 -0700

    updated style

--- src/Estimator.js
+++ src/Estimator.js
@@ -1,39 +1,43 @@
-import React from 'react'
-import { useHistory } from "react-router-dom";
+import React, {Fragment} from 'react';
+import { useHistory } from 'react-router-dom';
 
-import styles from './styles/estimatorStyles';
-import { withStyles } from '@material-ui/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-
-
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import HomeWorkIcon from '@material-ui/icons/HomeWork';
+import Divider from '@material-ui/core/Divider';
 
 function Estimator(props) {
-  const { classes } = props;
   const history = useHistory();
 
   const handleClick = (id) => {
-    history.push(`/estimate/${id}`)
-  
-  }
+    history.push(`/estimate/${id}`);
+  };
   return (
-    <Paper
-      style={{
-        padding: 0,
-        margin: 0,
-        height: '100vh',
-        backgroundColor: '#fafafa',
-      }}
-    >
-      <Grid container justify="center">
-        <Grid item xs={11} md={8} lg={4}>
-          { props.estimates.map( estimate => (
-            <Paper onClick={() => handleClick(estimate.id)} className={classes.paper}>{estimate.name}</Paper>
-          ))}
+    <Paper>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <List component="nav" aria-label="contacts">
+            {props.estimates.map((estimate) => (
+              <Fragment>
+                <ListItem onClick={() => handleClick(estimate.id)} button>
+                  <ListItemIcon>
+                    <HomeWorkIcon />
+                  </ListItemIcon>
+                  <ListItemText primary={`${estimate.name} ${estimate.address ? '@' : ''} ${estimate.address}`}
+                    secondary={`${estimate.note}`} />
+                </ListItem>
+                <Divider />
+                </Fragment>
+            ))}
+          </List>
         </Grid>
-        </Grid>
+      </Grid>
     </Paper>
-  )
+  );
 }
 
-export default withStyles(styles)(Estimator);
+export default Estimator;
