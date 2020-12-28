6aa77bf7f5f7474040b43b9276134914f1c37329
Eric Boisvenu <burn4live@gmail.com>
2020-12-28 13:54:57 -0700

    added styles

--- src/styles/estimateStyles.js
+++ src/styles/estimateStyles.js
@@ -2,8 +2,13 @@ const styles = {
   root: {
     flexGrow: 1,
     maxWidth: 650,
-    margin: 'auto'
+    margin: 'auto',
   },
-}
+  container: {
+    maxWidth: 700,
+    marginTop: '1em',
+    margin: 'auto',
+  },
+};
 
 export default styles;
