94d9a9014e13e5ef7f4cb18197ba2a671b03e03e
Eric Boisvenu <burn4live@gmail.com>
2020-12-28 13:59:07 -0700

    removed unused icon

--- src/MeasurementsForm.js
+++ src/MeasurementsForm.js
@@ -12,7 +12,6 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import AspectRatioIcon from '@material-ui/icons/AspectRatio';
 import ViewModuleIcon from '@material-ui/icons/ViewModule';
-import LabelImportantIcon from '@material-ui/icons/LabelImportant';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import ListItemText from '@material-ui/core/ListItemText';
 import Avatar from '@material-ui/core/Avatar';
