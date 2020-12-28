213347497e34e6e90f301160a6ade206b0b4a8ff
Eric Boisvenu <burn4live@gmail.com>
2020-12-28 13:54:47 -0700

    added reducers for material and hours

--- src/reducers/estimate.reducer.js
+++ src/reducers/estimate.reducer.js
@@ -4,24 +4,71 @@ const reducer = (state, action) => {
       return [
         ...state,
         {
-          ...action,
+          id: action.id,
+          name: action.name,
+          address: action.address,
+          note: action.note,
           measures: [],
+          hours: [],
+          material: [],
         },
       ];
     case 'REMOVE':
       return state.filter((estimate) => estimate.id !== action.id);
-    case 'DELMEASURE':
-      return state.filter((estimate) => estimate.id === action.id ? estimate.measures.splice(action.index, 1) : estimate);
     case 'EDIT':
       return state.map((estimate) =>
-        estimate.id === action.id
+          estimate.id === action.id
           ? {
               ...estimate,
               name: action.name,
               address: action.address,
               note: action.note,
+            }
+          : estimate
+      );
+      case 'ADDMEASURE':
+        return state.map((estimate) =>
+          estimate.id === action.id
+          ? {
+              ...estimate,
               measures: [...estimate.measures, action.measures],
             }
+          : estimate
+      );
+      case 'DELMEASURE':
+      return state.filter((estimate) =>
+        estimate.id === action.id
+          ? estimate.measures.splice(action.index, 1)
+          : estimate
+      );
+      case 'ADDHOURS':
+        return state.map((estimate) =>
+          estimate.id === action.id
+          ? {
+              ...estimate,
+              hours: [...estimate.hours, action.hours],
+            }
+          : estimate
+      );
+      case 'DELHOURS':
+      return state.filter((estimate) =>
+        estimate.id === action.id
+          ? estimate.hours.splice(action.index, 1)
+          : estimate
+      );
+      case 'ADDMATERIAL':
+        return state.map((estimate) =>
+          estimate.id === action.id
+          ? {
+              ...estimate,
+              material: [...estimate.material, action.material],
+            }
+          : estimate
+      );
+      case 'DELMATERIAL':
+      return state.filter((estimate) =>
+        estimate.id === action.id
+          ? estimate.material.splice(action.index, 1)
           : estimate
       );
     default:
