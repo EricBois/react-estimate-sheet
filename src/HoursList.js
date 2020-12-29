c61e5007a9d6bcc153a3f10e4f0e961c85036466
Eric Boisvenu <burn4live@gmail.com>
2020-12-29 15:32:19 -0700

    removed unused import

--- src/HoursList.js
+++ src/HoursList.js
@@ -5,7 +5,6 @@ import EditHoursForm from './EditHoursForm';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import LabelImportantIcon from '@material-ui/icons/LabelImportant';
-import ViewModuleIcon from '@material-ui/icons/ViewModule';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import ListItemText from '@material-ui/core/ListItemText';
 import Avatar from '@material-ui/core/Avatar';
