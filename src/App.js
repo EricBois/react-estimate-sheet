44f33d384105c47ee5dc167e377c1fd2a2ddd375
Eric Boisvenu <burn4live@gmail.com>
2021-01-03 10:44:12 -0700

    moved to estimateApp, removed localStorage

--- src/App.js
+++ src/App.js
@@ -1,29 +1,13 @@
 import React, { Fragment } from 'react';
-import { Route, Switch } from 'react-router-dom';
-import Estimator from './components/Estimator';
-import CreateEstimateForm from './components/CreateEstimateForm';
 import NavBar from './components/NavBar';
-import Estimate from './components/Estimate';
+import EstimateApp from './components/EstimateApp';
 import Welcome from './components/Welcome';
-import estimateReducer from './reducers/estimate.reducer';
-import useLocalStorageReducer from './hooks/useLocalStorageReducer';
 import { useSession } from './store/Session';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import Grid from '@material-ui/core/Grid';
 
 function App() {
   const { isLoggedIn, isLoading } = useSession();
-  const [estimates, dispatch] = useLocalStorageReducer(
-    'estimates',
-    [],
-    estimateReducer
-  );
-
-  function findEstimate(id) {
-    return estimates.find(function (estimate) {
-      return estimate.id === id;
-    });
-  }
 
   if (isLoading) {
     return (
@@ -38,36 +22,7 @@ function App() {
   return (
     <Fragment>
       {isLoggedIn ? (
-        <Route path="/">
-          <NavBar isLoggedIn={isLoggedIn} />
-          <Switch>
-            <Route exact path="/create">
-              <CreateEstimateForm
-                estimates={estimates}
-                dispatch={(props) => dispatch(props)}
-              />
-              ;
-            </Route>
-
-            <Route exact path="/">
-              <Estimator
-                estimates={estimates}
-                dispatch={(props) => dispatch(props)}
-              />
-            </Route>
-
-            <Route
-              exact
-              path="/estimate/:id"
-              render={(routeProps) => (
-                <Estimate
-                  estimate={findEstimate(routeProps.match.params.id)}
-                  dispatch={(props) => dispatch(props)}
-                />
-              )}
-            />
-          </Switch>
-        </Route>
+        <EstimateApp />
       ) : (
         <Fragment>
           <NavBar isLoggedIn={isLoggedIn} />
---
+++ src/components/EstimateApp.js
@@ -0,0 +1,88 @@
+import React, { Fragment, useEffect, useReducer } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Estimator from './Estimator';
+import CreateEstimateForm from './CreateEstimateForm';
+import NavBar from './NavBar';
+import Estimate from './Estimate';
+import estimateReducer from '../reducers/estimate.reducer';
+import { database } from "../firebase";
+import { useSession } from '../store/Session';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Grid from '@material-ui/core/Grid';
+
+function App() {
+  const { isLoggedIn, isLoading, currentUser } = useSession();
+  const [estimates, dispatch] = useReducer(estimateReducer, []);
+
+  useEffect(() => {
+    const fetchData = async() => {
+      try {
+
+        if (currentUser.uid) {
+          await database.collection('estimates').where("userId", "==", currentUser.uid).get().then(function(querySnapshot) {
+            querySnapshot.forEach(function(doc) {
+                // doc.data() is never undefined for query doc snapshots
+                dispatch({ type: 'ADD', ...doc.data() });
+            });
+          })
+        }
+        
+      } catch(e) {
+        console.log(e)
+      }
+    };
+    fetchData();
+  }, [currentUser])
+
+  function findEstimate(id) {
+    return estimates.find(function (estimate) {
+      return estimate.id === id;
+    });
+  }
+
+  if (isLoading) {
+    return (
+      <Grid direction="column" alignItems="center" justify="center" style={{ minHeight: '100vh' }} container>
+        <Grid item xs={12}>
+          <CircularProgress />
+        </Grid>
+      </Grid>
+    );
+  }
+
+  return (
+    <Fragment>
+        <Route path="/">
+          <NavBar isLoggedIn={isLoggedIn} />
+          <Switch>
+            <Route exact path="/create">
+              <CreateEstimateForm
+                estimates={estimates}
+                dispatch={(props) => dispatch(props)}
+              />
+            </Route>
+
+            <Route exact path="/">
+              <Estimator
+                estimates={estimates}
+                dispatch={(props) => dispatch(props)}
+              />
+            </Route>
+
+            <Route
+              exact
+              path="/estimate/:id"
+              render={(routeProps) => (
+                <Estimate
+                  estimate={findEstimate(routeProps.match.params.id)}
+                  dispatch={(props) => dispatch(props)}
+                />
+              )}
+            />
+          </Switch>
+        </Route>
+    </Fragment>
+  );
+}
+
+export default App;
