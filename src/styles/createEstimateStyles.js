3585f333091d42335cbb103e85c5fd82abeecf50
Eric Boisvenu <burn4live@gmail.com>
2021-01-06 20:08:48 -0700

    updated style

--- src/styles/createEstimateStyles.js
+++ src/styles/createEstimateStyles.js
@@ -1,10 +1,18 @@
 const styles = {
   textArea: {
-    width: '100%'
+    width: '100%',
+  },
+  paper: {
+    padding: '0px 15px',
+    margin: 'auto',
+    maxWidth: '850px',
+    height: '80vh',
+    backgroundColor: '#fafafa',
+    textAlign: 'center'
   },
   title: {
     padding: '5px',
-    fontWeight: 'bold'
-  }
-}
+    fontWeight: 'bold',
+  },
+};
 export default styles;
