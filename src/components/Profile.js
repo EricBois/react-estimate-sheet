e4a5c964a734006053b05f5c541c95fcb5eaa649
Eric Boisvenu <burn4live@gmail.com>
2021-01-13 16:00:15 -0700

    restored profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -8,45 +8,44 @@ import Divider from '@material-ui/core/Divider';
 import TextField from '@material-ui/core/TextField';
 
 function Profile(props) {
-  // const { database, currentUser, profile } = props;
-  // const { getUserFromDB } = useAuth();
   
-  // const [email, setEmail] = useInputState('');
-  // const [name, setName] = useInputState('');
+  const { profile } = props;
+  const [email, setEmail] = useInputState(profile.email || '');
+  const [name, setName] = useInputState(profile.displayName || '');
 
-  // return (
-  //   <Paper style={{maxWidth: '800px', margin: 'auto'}}>
-  //     <Grid container spacing={2}>
-  //       <Grid item xs={12}>
-  //         <Paper><Typography align="center" variant="h3" component="h2">Your Profile</Typography></Paper>
+  return (
+    <Paper style={{maxWidth: '800px', margin: 'auto'}}>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <Paper><Typography align="center" variant="h3" component="h2">Your Profile</Typography></Paper>
           
-  //       </Grid>
-  //       <Divider />
-  //       <Grid style={{ margin: 'auto' }} item xs={6}>
-  //           <TextField
-  //             fullWidth
-  //             value={name}
-  //             onChange={setName}
-  //             label="Name"
-  //             name="name"
-  //             size="small"
-  //             variant="outlined"
-  //           />
-  //         </Grid>
-  //         <Grid style={{ margin: 'auto' }} item xs={6}>
-  //           <TextField
-  //             fullWidth
-  //             value={email}
-  //             onChange={setEmail}
-  //             label="Email"
-  //             name="email"
-  //             size="small"
-  //             variant="outlined"
-  //           />
-  //         </Grid>
-  //     </Grid>
-  //   </Paper>
-  // )
+        </Grid>
+        <Divider />
+        <Grid style={{ margin: 'auto' }} item xs={6}>
+            <TextField
+              fullWidth
+              value={name}
+              onChange={setName}
+              label="Name"
+              name="name"
+              size="small"
+              variant="outlined"
+            />
+          </Grid>
+          <Grid style={{ margin: 'auto' }} item xs={6}>
+            <TextField
+              fullWidth
+              value={email}
+              onChange={setEmail}
+              label="Email"
+              name="email"
+              size="small"
+              variant="outlined"
+            />
+          </Grid>
+      </Grid>
+    </Paper>
+  )
 }
 
 export default Profile;
