ffd8d98b5523a845431ea98e3b8bd5efb1a82eed
Eric Boisvenu <burn4live@gmail.com>
2021-01-26 16:26:44 -0700

    added formik

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import useInputState from '../hooks/useInputState';
+import React, {useState} from 'react';
 import firebase from "../firebase";
+import { useFormik } from "formik";
 
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
@@ -10,18 +10,49 @@ import Paper from '@material-ui/core/Paper';
 
 function SignIn(props) {
 
-  const [email, setEmail] = useInputState('');
-  const [password, setPassword] = useInputState('');
+  const [error, setError] = useState(null);
   const { toggle } = props;
 
+  const validate = values => {
+    const errors = {};
+    if (!values.email) {
+      errors.email = "Required";
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(formik.values.email)) {
+      errors.email = 'Invalid Email Address';
+    }
+  
+    if (!values.password) {
+      errors.password = "Required";
+    } else if (values.password.length <= 6) {
+      errors.password = 'Must be more than 6 characters';
+    }
+  
+    return errors;
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: ""
+    },
+    validate,
+    onSubmit: (values, {resetForm}) => {
+      firebase.login(values.email, values.password).then(
+        setError(null),
+        resetForm({ values: ''})
+      ).catch((err) => {
+        setError('Email or Password Invalid')
+      })
+      resetForm({ values: ''})
+    }
+  });
 
   return (
     <Paper style={{minHeight: '100vh', maxWidth: '400px', margin: 'auto'}}>
       <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          firebase.login(email, password)
-        }}
+        onSubmit={
+          formik.handleSubmit
+        }
       >
         <Grid container  spacing={2}>
           <Grid item style={{ margin: 'auto' }} xs={10}>
@@ -32,27 +63,32 @@ function SignIn(props) {
           <Grid style={{ margin: 'auto' }} item xs={10}>
             <TextField
               fullWidth
-              value={email}
-              onChange={setEmail}
+              value={formik.values.email}
+              onChange={formik.handleChange}
               label="Email"
               name="email"
               size="small"
+              required
               variant="outlined"
             />
+            {formik.errors.email ? <div>{formik.errors.email}</div> : null}
           </Grid>
           <Grid style={{ margin: 'auto' }} item xs={10}>
             <TextField
               fullWidth
-              value={password}
-              onChange={setPassword}
+              value={formik.values.password}
+              onChange={formik.handleChange}
+              required
               label="Password"
               name="password"
               size="small"
               type="password"
               variant="outlined"
             />
+            {formik.errors.password ? <div>{formik.errors.password}</div> : null}
           </Grid>
           <Grid style={{ margin: 'auto' }} item xs={10}>
+            {error ? <p style={{color: 'red', margin: 'auto', textAlign: 'center'}}>{error}</p>: null}
             <Button
               color="secondary"
               fullWidth
