cf21a04b26a6dd87f96d9656e82957a67dacb7ff
burn4live <ericb@linux.com>
2021-07-04 16:40:21 -0600

    added link

--- src/components/EstimateList.js
+++ src/components/EstimateList.js
@@ -8,6 +8,8 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import Divider from '@material-ui/core/Divider';
 import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import LibraryAddSharpIcon from '@material-ui/icons/LibraryAddSharp';
 
 function EstimateList(props) {
   const { estimates, dispatch } = props;
@@ -15,8 +17,9 @@ function EstimateList(props) {
   return (
     <Paper elevation={20} style={{ maxWidth: '600px', minHeight: '200px', margin: 'auto' }}>
       <Grid container spacing={2}>
-        <Grid item xs={12}>
+        <Grid item xs={12} style={{textAlign: 'right'}}>
           <Typography align="center" variant="h5" style={{fontFamily: 'Russo One'}}>Estimate List</Typography>
+          <IconButton component={Link} to="/create" style={{marginTop: '-3.4rem', marginRight: '1rem'}}><LibraryAddSharpIcon /></IconButton>
           <Divider />
           <List component="nav" aria-label="contacts">
             {estimates.length === 0 && (
