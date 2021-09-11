4b2488eddc5eb8e46e652637517750fad898e722
burn4live <ericb@linux.com>
2021-09-10 20:22:31 -0600

    fixed grid to center

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -2,17 +2,25 @@ import React from 'react';
 import useToggleState from './hooks/useToggleState';
 import SignIn from './SignIn';
 import Signup from './SignUp';
+import Grid from '@material-ui/core/Grid';
 
 export default function Welcome() {
   const [isNew, toggle] = useToggleState(false);
   return (
-    <div>
+    <Grid
+  container
+  spacing={0}
+  direction="column"
+  alignItems="center"
+  justify="center"
+  style={{ minHeight: '60vh' }}
+>
       { !isNew ?
         <SignIn toggle={() => toggle}/> :
         <Signup toggle={() => toggle}/>
       }
       
       
-    </div>
+    </Grid>
   )
 }
