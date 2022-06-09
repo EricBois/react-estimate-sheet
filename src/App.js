7c29664c43b2aba66937eb7982e655dbfb028764
Eric Boisvenu <burn4live@gmail.com>
2022-06-09 08:21:37 -0600

    fix: fixed loading

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React, { Fragment, useEffect, useState } from 'react';
-import NavBar from './components/NavBar';
-import { BrowserRouter } from 'react-router-dom';
-import EstimateApp from './components/EstimateAppRouter';
-import Welcome from './components/Welcome';
-import Loader from './components/Loader';
-import useAuth from './components/Auth/useAuth';
-import {useSettings} from './components/hooks/useSettings';
-import firebase, { FirebaseContext } from './firebase/index';
+import React, { Fragment } from "react";
+import NavBar from "./components/NavBar";
+import { BrowserRouter } from "react-router-dom";
+import EstimateApp from "./components/EstimateAppRouter";
+import Welcome from "./components/Welcome";
+import Loader from "./components/Loader";
+import useAuth from "./components/Auth/useAuth";
+import { useSettings } from "./components/hooks/useSettings";
+import firebase, { FirebaseContext } from "./firebase/index";
 
 function App() {
-  const user = useAuth();
-  const {settings} = useSettings();
-  const [isLoading, setLoading] = useState(true);
-
-  useEffect(() => {
-    setTimeout(() => setLoading(false), 500);
-  }, []);
+  const { user, isLoading } = useAuth();
+  const { settings } = useSettings();
 
   if (isLoading) {
-    return (
-      <Loader />
-    );
+    return <Loader />;
   }
 
   return (
--- src/components/Auth/useAuth.js
+++ src/components/Auth/useAuth.js
@@ -1,22 +1,22 @@
-import { useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import firebase from "../../firebase/index";
 
-function useAuth() {
+export default function useAuth() {
   const [authUser, setAuthUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    const unsubscribe = firebase.auth.onAuthStateChanged(user => {
+    const unsubscribe = firebase.auth.onAuthStateChanged((user) => {
       if (user) {
         setAuthUser(user);
       } else {
         setAuthUser(null);
       }
+      setIsLoading(false);
     });
 
     return () => unsubscribe();
   }, []);
 
-  return authUser;
+  return { authUser, isLoading };
 }
-
-export default useAuth;
